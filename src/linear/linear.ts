8906189481ac7d3ee65e7dc6d8c4822893737c39
Anubhav Saini <iamanubhavsaini+git@gmail.com>
2025-04-22 22:05:13 +0530

    v0.0.1

---
+++ src/linear/linear.ts
@@ -0,0 +1,52 @@
+class Linear extends HTMLElement {
+    direction: "vertical" | "horizontal";
+    uniqueKey: string;
+    height: string;
+    width: string;
+
+    constructor() {
+        super();
+        this.attachShadow({ mode: "open" });
+        this.uniqueKey = this.getAttribute("unique-key") || "default-linear-layout-";
+        this.direction = this.getAttribute("direction") === "horizontal" ? "horizontal" : "vertical";
+        this.height = this.getAttribute("height") || "max-content";
+        this.width = this.getAttribute("width") || "100dvw";
+        console.log(this.height, this.width);
+    }
+
+    async connectedCallback(): Promise<void> {
+        const gap = this.getAttribute("gap") || "4rem";
+        const classes = this.getAttribute('classes') || "";
+        const margin_tb = `${(this.getAttribute("margin:tb") || "0") + "rem"}`;
+        const margin_rl = `${(this.getAttribute("margin:rl") || "0") + "rem"}`;
+
+        if (!this.shadowRoot) { return; }
+
+        this.shadowRoot.innerHTML = `
+        <style>
+            .linear {
+                height: ${this.height};
+                width: ${this.width};
+                display: flex;
+                flex-direction: ${this.direction === "horizontal" ? "row" : "column"};
+                gap: ${gap};
+                margin: ${margin_tb} ${margin_rl};
+            }
+        </style>
+        <slot class="linear ${classes}"></slot>
+        `;
+    }
+
+}
+
+function registerLinear(): void {
+    if (!customElements.get('layout-linear')) {
+        customElements.define('layout-linear', Linear);
+    }
+}
+
+export default { registerLinear, Linear }
+
+
+// <div class="linear ${classes}">
+// </div>
